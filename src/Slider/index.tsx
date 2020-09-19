d99643db8974fa813e7f168d9c13bacebadae36c
Miguel Andrade Barreto <miguelandradebarreto2@gmail.com>
2020-09-19 20:00:31 -0300

    :rocket: swipe trail

--- src/Slider/index.tsx
+++ src/Slider/index.tsx
@@ -1,32 +1,55 @@
 import React, { useState } from 'react'
 import { Container, Card } from './styles'
 
-import { useSpring } from 'react-spring'
+import * as d3 from 'd3-ease'
+import { useTrail } from 'react-spring'
 import { useDrag } from 'react-use-gesture'
 
 function clamp(value: number, min: number, max: number) {
   return Math.min(Math.max(value, min), max)
 }
 
+const cards = [
+  {
+    name: 'personal',
+    key: 1,
+  },
+  {
+    name: 'student',
+    key: 1,
+  },
+  {
+    name: 'professor',
+    key: 1,
+  },
+]
+
 const Slider: React.FC = () => {
   const [position, setPosition] = useState(0)
 
-  const { x } = useSpring({ x: position * 300 })
+  const trail = useTrail(3, {
+    x: position * 600,
+    delay: 0,
+    config: { mass: 1 },
+  })
 
   const bind = useDrag(
     ({ swipe: [swipeX] }) => {
       setPosition(prevPosition => clamp(prevPosition + swipeX, -1, 1))
     },
     {
-      swipeDistance: [50, 50],
-      swipeVelocity: [0.3, 0.3],
+      swipeDistance: 0,
+      swipeVelocity: 0,
     }
   )
 
-  console.log(position)
   return (
     <Container>
-      <Card {...bind()} style={{ x }} />
+      {trail.map((props, index) => (
+        <Card key={cards[index].key} {...bind()} style={props}>
+          {cards[index].name}
+        </Card>
+      ))}
     </Container>
   )
 }
--- src/Slider/styles.ts
+++ src/Slider/styles.ts
@@ -4,12 +4,25 @@ import { animated } from 'react-spring'
 export const Card = styled(animated.div)`
   width: 500px;
   height: 600px;
+  font-size: 2rem;
 
-  background-color: black;
+  background-image: linear-gradient(135deg, #f093fb 0%, #f5576c 100%);
+  color: white;
+  box-shadow: 0px 10px 25px -10px rgba(0, 0, 0, 0.2);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  user-select: none;
 `
 
 export const Container = styled(animated.div)`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  ${Card} + ${Card} {
+    margin-left: 20px;
+  }
 `
